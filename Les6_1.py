449e2dadbfe79e7d451e4e5bed0df1c2ae35e0e9
Okzorom <72410124+Okzorom@users.noreply.github.com>
2020-11-13 17:42:32 +0300

    Update Les6_1.py
    add the pause

--- Les6_1.py
+++ Les6_1.py
@@ -1,11 +1,13 @@
 import itertools
+import time
 
 class TrafficLight:
     def __init__(self):
         self.__color = (('red', 5), ('yellow', 2), ('green', 28))
     def running(self):
         for color, seconds in itertools.cycle(self.__color):
-            print(color, '`(U needa wait {} seconds'.format(seconds))
+            print(color, '`(U needa wait {} seconds)'.format(seconds))
+            time.sleep(seconds)
 trafficlight = TrafficLight()
 
 trafficlight.running()
